ea6860c29cce0ef20a67ed545f080584a5904bae
Aaron Feldman <feldmando@gmail.com>
2014-01-06 16:50:37 -0800

    some regular expression stuff to find links

--- download.py
+++ download.py
@@ -1,18 +1,26 @@
 from urllib2 import urlopen
 from urllib import urlretrieve
 import csv
+import re
 
 with open('personalize.csv', 'rb') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     personal = list(csvreader)
     # personal is now a 2D list. access[row][column]
 
-link = 'http://www.math.caltech.edu/~2013-14/2term/ma108b/hwk1.pdf'
-link2 = 'http://www.math.caltech.edu/~2013-14/2term/ma108b'
-url = urlopen(link2)
+p = re.compile('href.*\".*pdf\"')
+
+link = 'http://www.math.caltech.edu/~2013-14/2term/ma108b'
+link2 = 'http://www.math.caltech.edu/~2013-14/2term/ma108b/hwk1.pdf'
+url = urlopen(link)
 text = url.read()
+
 # search text for hrefsomething"something.pdf"
 # get the link and set it as LINK
 # get the name and set it as NAME. also add location info to the name
+
 name = '/home/aaron/Dropbox/Caltech/WIN_2014/hwk1.pdf'
-# urlretrieve(link, name)
+# urlretrieve(link2, name)
+
+filelist = p.findall(text)
+print filelist
