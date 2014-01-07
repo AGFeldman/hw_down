b45e67ce99d8e1351b431b2e5fcffdd80c31e7b3
Aaron Feldman <feldmando@gmail.com>
2014-01-06 17:30:15 -0800

    stuff about getting information from personalize.csv

--- download.py
+++ download.py
@@ -4,6 +4,14 @@ import csv
 import re
 
 typesline = 1
+placesline = 4
+
+def isgood(entry):
+    if entry == '':
+        return False
+    elif entry == ',':
+        return False
+    return True
 
 with open('personalize.csv', 'rb') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -13,13 +21,31 @@ with open('personalize.csv', 'rb') as csvfile:
 typeslist = []
 reglist = []
 for entry in personal[typesline]:
-    if '' != entry != ',':
+    if isgood(entry):
         typeslist.append(entry)
         reglist.append('href.*\".*' + entry + '\"')
 finalreg = '|'.join(reglist)
-print finalreg
+linkre = re.compile(finalreg)
+# linkre is now a regular expression that matches any html code snippet
+# that is a link to a file of one of the types specified in personalize.csv
 
-p = re.compile(finalreg)
+placeslist = []
+for row in personal[placesline:]:
+    place = row[0]
+    if not isgood(place):
+        continue
+    webs = []
+    for entry in row[1:]:
+        if isgood(entry):
+            webs.append(entry)
+    placeslist.append((place, webs))
+# placeslist is now a list of tuples
+# (place on your computer, [list of web pages to scan there])
+
+def download(place, website):
+    html = urlopen(website).read()
+    filelist = linkre.findall(html)
+    
 
 link = 'http://www.math.caltech.edu/~2013-14/2term/ma108b'
 link2 = 'http://www.math.caltech.edu/~2013-14/2term/ma108b/hwk1.pdf'
@@ -33,5 +59,5 @@ text = url.read()
 name = '/home/aaron/Dropbox/Caltech/WIN_2014/hwk1.pdf'
 # urlretrieve(link2, name)
 
-filelist = p.findall(text)
+filelist = linkre.findall(text)
 print filelist
